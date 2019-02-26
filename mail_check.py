79ade51ec8e8ed8d82bcf945bcfa80c3b0a3d158
akra <trustyman77@hotmail.com>
2019-02-26 03:20:43 -0800

    some errors and logging added

--- mail_check.py
+++ mail_check.py
@@ -15,7 +15,8 @@ secretkey = 'wN9lCCQla2DRvkQZ60jX0-ImGEY'
 botname = 'RonTips4Liberty'
 bot_pw = 'Serbialovescrypto2019!'
 bot_agent = 'bot by vladan'
-subreddit_name = 'RonTips4Liberty'
+# subreddit_name = 'RonTips4Liberty'
+mail_log_file = 'mail_log.txt' # log file for mail_check
 
 # look for phrase and reply appropriately
 def bot_login():
@@ -29,50 +30,58 @@ def bot_login():
     return reddit
 
 def run_bot(reddit):
-    print "Searching unread messages"
-    unread_messages = []
-    for item in reddit.inbox.unread(limit=None):
-        if isinstance(item, Message):
-            unread_messages.append(item)
-            print 'command message: {0}'.format(item.body)
-            accountStr = 'reddit-{0}'.format(item.author.name)
-            print 'wallet accountname: {0}'.format(accountStr)
-            if 'balance' in item.body.lower():
-                if len(item.body.split())==1:
-                    # get balance data by running daemon ronpaulcoind
-                    balance = subprocess.check_output([core,"getbalance", accountStr])[:-1]
-                    reddit.redditor(item.author.name).message('Balance', balance)
-                else:
-                    reddit.redditor(item.author.name).message('Balance error', 'Usage in PM: `balance`')
-            elif 'deposit' in item.body.lower():
-                if len(item.body.split())==1:
-                    deposit = subprocess.check_output([core,"getaccountaddress", accountStr])[:-1]
-                    reddit.redditor(item.author.name).message('Deposit', "Your depositing address is: {0}".format(deposit))
-                else:
-                    reddit.redditor(item.author.name).message('Deposit error', "Usage in PM: `deposit`")
-            elif 'withdraw' in item.body.lower():
-                if len(item.body.split())==3:
-                    try:
-                        amount = float(item.body.split()[1])
-                        address = item.body.split()[2]
-                        accountStr = 'reddit-{0}'.format(item.author.name)
-                        balance = float(subprocess.check_output([core,"getbalance", accountStr])[:-1])
-                        if balance < amount:
-                            reddit.redditor(item.author.name).message('Withdraw error', "You have insufficent funds.")
-                        else:
-                            amount = str(amount)
-                            # sendfrom <fromaccount> <toronpaulcoinaddress> <amount> [minconf=1] [comment] [comment-to]
-                            tx = subprocess.check_output([core,"sendfrom",accountStr,address,amount])[:-1]
-                            # {0} has successfully withdrew to address: {1} of {2} RDD"
-                            reddit.redditor(item.author.name).message('Withdraw success', "You have successfully withdrew to address: {0} of {1} RPC".format(address,amount))
-                    except ValueError:
+    with open(mail_log_file, "a") as logfile:
+        logfile.write("Searching unread messages\n")
+        unread_messages = []
+        for item in reddit.inbox.unread(limit=None):
+            if isinstance(item, Message):
+                unread_messages.append(item)
+                logfile.write('command message: {0}'.format(item.body))
+                accountStr = 'reddit-{0}'.format(item.author.name.lower())
+                logfile.write('wallet accountname: {0}'.format(accountStr))
+                if 'balance' in item.body.lower():
+                    if len(item.body.split())==1:
+                        # get balance data by running daemon ronpaulcoind
+                        balance = subprocess.check_output([core,"getbalance", accountStr])[:-1]
+                        reddit.redditor(item.author.name).message('Balance', balance)
+                        logfile.write('balance of {0} is {1}\n'.format(item.author.name.lower(), balance))
+                    else:
+                        reddit.redditor(item.author.name).message('Balance error', 'Usage in PM: `balance`')
+                        logfile.write('Balance error: \nUsage in PM: `balance`\n')
+                elif 'deposit' in item.body.lower():
+                    if len(item.body.split())==1:
+                        deposit = subprocess.check_output([core,"getaccountaddress", accountStr])[:-1]
+                        reddit.redditor(item.author.name).message('Deposit', "Your depositing address is: {0}".format(deposit))
+                        logfile.write('Deposit\nYour depositing Your depositing address is: {0}'.format(deposit))
+                    else:
+                        reddit.redditor(item.author.name).message('Deposit error', "Usage in PM: `deposit`")
+                        logfile.write('Deposit error\nUsage in PM: `deposit`')
+                elif 'withdraw' in item.body.lower():
+                    if len(item.body.split())==3:
+                        try:
+                            amount = float(item.body.split()[1])
+                            address = item.body.split()[2]
+                            # accountStr = 'reddit-{0}'.format(item.author.name)
+                            balance = float(subprocess.check_output([core,"getbalance", accountStr])[:-1])
+                            if balance < amount:
+                                reddit.redditor(item.author.name).message('Withdraw error', "You have insufficent funds.")
+                                logfile.write('Withdraw error\nYou have insufficent funds.')
+                            else:
+                                amount = str(amount)
+                                # sendfrom <fromaccount> <toronpaulcoinaddress> <amount> [minconf=1] [comment] [comment-to]
+                                tx = subprocess.check_output([core,"sendfrom",accountStr,address,amount])[:-1]
+                                # {0} has successfully withdrew to address: {1} of {2} RDD"
+                                reddit.redditor(item.author.name).message('Withdraw success', "You have successfully withdrew to address: {0} of {1} RPC".format(address,amount))
+                                logfile.write('Withdraw success\nYou have successfully withdrew to address: {0} of {1} RPC'.format(address,amount))
+                        except ValueError:
+                            reddit.redditor(item.author.name).message('Withdraw error', "Usage in PM: `withdraw <amount> <address>`")
+                            logfile.write('Withdraw error\nUsage in PM: `withdraw <amount> <address>`')
+                    else:
                         reddit.redditor(item.author.name).message('Withdraw error', "Usage in PM: `withdraw <amount> <address>`")
-                else:
-                    reddit.redditor(item.author.name).message('Withdraw error', "Usage in PM: `withdraw <amount> <address>`")
+                        logfile.write('Withdraw error\nUsage in PM: `withdraw <amount> <address>`')
     reddit.inbox.mark_read(unread_messages)
     reddit.inbox.collapse(unread_messages)
     time.sleep(10)
-
 
 reddit = bot_login()
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 import os
 import config
+import requests
 
 #daemon core
 core = "/home/RonTipsProject/ronpaulcoind"
@@ -14,7 +15,10 @@ secretkey = 'wN9lCCQla2DRvkQZ60jX0-ImGEY'
 botname = 'RonTips4Liberty'
 bot_pw = 'Serbialovescrypto2019!'
 bot_agent = 'bot by vladan'
-subreddit_name = 'RonTips4Liberty'
+# subreddit_name = 'RonTips4Liberty'
+
+subreddit_list_file = 'subreddit_list.txt' # file name of subreddit's list
+comment_log_file = 'comment_log.txt' # file name of comment log
 
 # look for phrase and reply appropriately
 def bot_login():
@@ -27,92 +31,95 @@ def bot_login():
     print "Logged in!"
     return reddit
 
-def run_bot(reddit, comments_replied_to):
-    print "Searching last 1,000 comments"
-    for comment in reddit.subreddit(subreddit_name).comments(limit=1000):
-        # print "********************"
-        # print "comment.parent_id:"
-        # print comment.parent_id
-        # print "comment.body:"
-        # print comment.body
-        # # print "comment.comments_replied_to"
-        # # print comments_replied_to
-        # print "comment.author"
-        # print comment.author
-        # print "reddit.user.me"
-        # print reddit.user.me()
-        # comment.mod.remove()
-        if comment.id not in comments_replied_to:
-            try:
-                commandline = comment.body
-                print 'commandline: {0}'.format(commandline)
-                # print "tip:"
-                # print "{0}abcdefg".format(commandline.split()[0])
-                if 'tip' in commandline.lower():
-                    comments_replied_to.append(comment.id)
-                    with open ("comments_replied_to.txt", "a") as f:
-                        f.write(comment.id + "\n")
-                    print "tip(comment)"
-                    tip(reddit, comment)
-                # elif commandline.split()[0] == 'gild':
-                #     # gild(commandline)
-            except:
-                print('to frequent')
-    print "Sleeping for 10 seconds..."
-    #Sleep for 10 seconds...
-    time.sleep(10)
+def run_bot(reddit, comments_replied_to, subreddits_list):
+    with open(comment_log_file, "a") as logfile:
+        logfile.write("Searching last 1,000 comments\n")
+        print "searching"
+        for item in subreddits_list:
+            logfile.write("subreddit name: {0}\n".format(item))
+            for comment in reddit.subreddit(item).comments(limit=1000):
+                if comment.id not in comments_replied_to:
+                    try:
+                        commandline = comment.body
+                        logfile.write('commandline: {0}\n'.format(commandline))
+                        # print "tip:"
+                        # print "{0}abcdefg".format(commandline.split()[0])
+                        if 'tip' in commandline.lower():
+                            comments_replied_to.append(comment.id)
+                            with open ("comments_replied_to.txt", "a") as f:
+                                f.write(comment.id + "\n")
+                            print "tip(comment)"
+                            tip(reddit, comment, logfile)
+                        # elif commandline.split()[0] == 'gild':
+                        #     # gild(commandline)
+                    except:
+                        logfile.write('to frequent\n')
+            logfile.write("Sleeping for 10 seconds...\n")
+            #Sleep for 10 seconds...
+        time.sleep(10)
 
 
-def tip(reddit, comment):
-    print 'it is inside tip function'
+def tip(reddit, comment, logfile):
+    logfile.write('it is inside tip function\n')
     sender = comment.author.name
     if len(comment.body.split()) == 3:
         amount = comment.body.split()[1]
         try:
             amount = float(amount)
-            receiver = comment.body.split()[2]
+            receiver = comment.body.lower().split()[2]
             if receiver[0]=='/' and receiver[1]=='u' and receiver[2]=='/':
-                receiver = receiver[3:]
-                senderStr = 'reddit-{0}'.format(sender)
-                receiverStr = 'reddit-{0}'.format(receiver)
-                result = subprocess.check_output([core,"getbalance", senderStr])[:-1]
-                balance = float(result)
-                print "balance:"
-                print balance
-                print "---------------"
-                print "receiver:"
-                print receiverStr
-                print "sender:"
-                print senderStr
-                if balance < amount:
-                    reddit.redditor(sender).message('Tip error', "{0}, you have insufficent funds.".format(sender))
-                    print "{0}, you have insufficent funds.".format(sender)
-                elif receiver == sender:
-                    # comment.reply("You can't tip yourself silly.")
-                    reddit.redditor(sender).message('Tip error', "You can't tip yourself silly.")
-                    print "You can't tip yourself silly."
-                else:
-                    balance = str(balance)
-                    amount = str(amount)
-                    # check if the receiver has wallet account
-                    addresses = subprocess.check_output([core, "getaddressesbyaccount", receiverStr])
-                    if len(addresses.split())==2:
-                        subprocess.check_output([core,"getaccountaddress", receiverStr])
-                    # send coin to custom reddit user
-                    tx = subprocess.check_output([core,"move",senderStr,receiverStr,amount])[:-1]
-                    # comment.reply("@{0} tipped @{1}RPC to @{2}".format(sender, amount, receiver))
-                    # reddit.redditor(sender).message('Tip', "@{0} tipped @{1}RPC to @{2}".format(sender, amount, receiver))
-                    reddit.redditor(receiver).message('Tipped', "{0} tipped {1}RPC to {2}".format(sender, amount, receiver))
-                    print "{0} tipped {1}RPC to {2}".format(sender, amount, receiver)
+                try:
+                    receiver = receiver[3:]
+                    validflag = requests.get('https://www.reddit.com/user/{0}.json'.format(receiver))
+                    try:
+                        if validflag.error != 404:
+                            senderStr = 'reddit-{0}'.format(sender)
+                            receiverStr = 'reddit-{0}'.format(receiver)
+                            result = subprocess.check_output([core,"getbalance", senderStr])[:-1]
+                            balance = float(result)
+                            logfile.write("balance:\n{0}".format(balance))
+                            logfile.write("---------------\n")
+                            logfile.write("receiver:\n{0}".format(receiverStr))
+                            logfile.write("sender:\n{0}".format(senderStr))
+                            if balance < amount:
+                                reddit.redditor(sender).message('Tip error', "{0}, you have insufficent funds.".format(sender))
+                                logfile.write("{0}, you have insufficent funds.".format(sender))
+                            elif receiver == sender:
+                                # comment.reply("You can't tip yourself silly.")
+                                reddit.redditor(sender).message('Tip error', "You can't tip yourself silly.")
+                                logfile.write("You can't tip yourself silly.")
+                            else:
+                                balance = str(balance)
+                                amount = str(amount)
+                                # check if the receiver has wallet account
+                                addresses = subprocess.check_output([core, "getaddressesbyaccount", receiverStr])
+                                if len(addresses.split())==2:
+                                    subprocess.check_output([core,"getaccountaddress", receiverStr])
+                                # send coin to custom reddit user
+                                tx = subprocess.check_output([core,"move",senderStr,receiverStr,amount])[:-1]
+                                # comment.reply("@{0} tipped @{1}RPC to @{2}".format(sender, amount, receiver))
+                                # reddit.redditor(sender).message('Tip', "@{0} tipped @{1}RPC to @{2}".format(sender, amount, receiver))
+                                reddit.redditor(receiver).message('Tipped', "{0} tipped {1}RPC to {2}".format(sender, amount, receiver))
+                                logfile.write("{0} tipped {1}RPC to {2}".format(sender, amount, receiver))
+                        else:
+                            reddit.redditor(sender).message('Tip error', "{0} is not valid reddit user.".format(receiver))
+                            logfile.write("Tip error\n{0} is not valid reddit user.".format(receiver))
+                    except:
+                        reddit.redditor(sender).message('Tip error', "{0} is not valid reddit user.".format(receiver))
+                        logfile.write("Tip error\n{0} is not valid reddit user.".format(receiver))
+                except Exception as ex:
+                    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                    message = template.format(type(ex).__name__, ex.args)
+                    logfile.write(message)
             else:
                 reddit.redditor(sender).message('Tip accountname format error', "Usage in comment: `tip <amount> /u/<username>`".format(sender))
-                print 'Tip error: {0}'.format(comment.body)
+                logfile.write('Tip error: {0}'.format(comment.body))
         except ValueError:
             reddit.redditor(sender).message('Tip error', "Usage in comment: `tip <amount> /u/<username>`".format(sender))
-            print 'Tip amount error: {0}'.format(comment.body)
+            logfile.write('Tip amount error: {0}'.format(comment.body))
     else:
         reddit.redditor(sender).message('Tip error', "Usage in comment: `tip <amount> /u/<username>`".format(sender))
-        print 'Tip format error: {0}'.format(comment.body)
+        logfile.write('Tip format error: {0}'.format(comment.body))
 
 def get_saved_comments():
     if not os.path.isfile("comments_replied_to.txt"):
@@ -124,9 +131,22 @@ def get_saved_comments():
             comments_replied_to = filter(None, comments_replied_to)
     return comments_replied_to
 
+def get_subreddit_list(filename):
+    print "Reading subreddits from {0} file".format(filename)
+    if not os.path.isfile(filename):
+        subreddits_list = []
+    else:
+        with open(filename, "r") as f:
+            subreddits_list = f.read()
+            subreddits_list = subreddits_list.split("\n")
+            subreddits_list = filter(None, subreddits_list)
+            print subreddits_list
+    return subreddits_list
+
 reddit = bot_login()
+subreddits_list = get_subreddit_list(subreddit_list_file)
 comments_replied_to = get_saved_comments()
 # print comments_replied_to
 
 while True:
-    run_bot(reddit, comments_replied_to)
+    run_bot(reddit, comments_replied_to, subreddits_list)
